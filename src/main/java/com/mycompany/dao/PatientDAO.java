91ddab133755bf9de24ed275709eb4e8516321fe
badrhrm <badr.eddine.hrimech@proton.me>
2024-05-12 09:32:51 +0100

    finished all patient Dao functions

--- src/main/java/com/mycompany/dao/PatientDAO.java
+++ src/main/java/com/mycompany/dao/PatientDAO.java
@@ -3,6 +3,8 @@ package com.mycompany.dao;
 import com.mycompany.models.Patient;
 import com.mycompany.utils.HibernateUtil;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -25,6 +27,52 @@ public class PatientDAO {
             transaction = session.beginTransaction();
             
             session.save(patient);
+            
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    public List<Patient> getAllPatients() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM Patient", Patient.class)
+                    .list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public boolean updatePatient(Patient patient) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            
+            session.update(patient);
+            
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deletePatient(Patient patient) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            
+            session.delete(patient);
             
             transaction.commit();
             return true;
